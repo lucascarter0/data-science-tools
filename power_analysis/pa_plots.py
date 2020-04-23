ba6afd65fac809b780d57509a7501935cb5a3262
Lucas Carter <38087187+lucascarter0@users.noreply.github.com>
2020-04-23 17:59:08 -0500

    Add files via upload
    Updating scripts to be more readable.

--- power_analysis/pa_plots.py
+++ power_analysis/pa_plots.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 17 13:09:21 2020
+Created on Fri Feb 16 13:09:21 2018
 
 @author: lucas
 """
@@ -20,15 +20,14 @@ gauss = Test()
 
 # Distribution is Gaussian
 gauss.dist = lambda m,n : np.random.normal(loc=m, size=(n,))
-# Two sample KS hypothesis test
-gauss.hypothesis = stats.ks_2samp
 
 # mean value of Gaussian
 m = np.array([0])
 
 
 
-#Compute power statistic
+#Compute power statistic for two sample KS hypothesis test
+gauss.hypothesis = stats.ks_2samp
 power = montecarlo_sweep(gauss, means=m, effect_sizes=effect)
 
 # Plot results
@@ -36,7 +35,7 @@ title = 'Statistical Power of KS Test\n'\
 'Gaussian Distribution ($\mu$=0)\n'\
 'Various Effect Sizes (Percentage of Original Mean Value)'
 contour_plot(gauss, effect, power, ylabel='Effect Size',
-             title=title, save_label='ks_gaussian')
+             title=title, save_label='gaussian_ks')
 
 # Repeat with two sample t-test (assumes equal variance)
 gauss.hypothesis = stats.ttest_ind
@@ -47,10 +46,10 @@ title = 'Statistical Power of T-Test\n'\
 'Gaussian Distribution ($\mu$=0)\n'\
 'Various Effect Sizes (Percentage of Original Mean Value)'
 contour_plot(gauss, effect, power, ylabel='Effect Size', 
-             title=title, save_label='ttest_gaussian')
+             title=title, save_label='gaussian_ttest')
 
 
-# Repeat with two sample t-test (assumes equal variance)
+# Repeat with Wilcoxon signed-rank test
 gauss.hypothesis = stats.wilcoxon
 power = montecarlo_sweep(gauss, means=m, effect_sizes=effect)
 
@@ -59,13 +58,35 @@ title = 'Statistical Power of Wilcoxon Signed-Rank Test\n'\
 'Gaussian Distribution ($\mu$=0)\n'\
 'Various Effect Sizes (Percentage of Original Mean Value)'
 contour_plot(gauss, effect, power, ylabel='Effect Size', 
-             save_label='wilcoxon_gaussian', title=title)
+             save_label='gaussian_wilcoxon', title=title)
+
+
+# Repeat with Mann-Whitney U test
+gauss.hypothesis = stats.mannwhitneyu
+power = montecarlo_sweep(gauss, means=m, effect_sizes=effect)
+
+# Plot results
+title = 'Statistical Power of Mann-Whitney U Test\n'\
+'Gaussian Distribution ($\mu$=0)\n'\
+'Various Effect Sizes (Percentage of Original Scale Value)'
+contour_plot(gauss, effect, power, ylabel='Effect Size', 
+             save_label='gaussian_mannwhitney', title=title)
+
+
+# Repeat with Kruskal-Wallis H test
+gauss.hypothesis = stats.kruskal
+power = montecarlo_sweep(gauss, means=m, effect_sizes=effect)
+
+# Plot results
+title = 'Statistical Power of Kruskal-Wallis H Test\n'\
+'Gaussian Distribution ($\mu$=0)\n'\
+'Various Effect Sizes (Percentage of Original Scale Value)'
+contour_plot(gauss, effect, power, ylabel='Effect Size', 
+             save_label='gaussian_kruskalwallis', title=title)
 
 
 
-
-
-
+# Binomial Distribution
 binom = Test()
 binom.dist = lambda p, n : np.random.binomial(1, p, size=(n,))
 binom.hypothesis = lambda a,b, : mcnemar(contingency_table(a,b), exact=True)
@@ -81,4 +102,71 @@ title = 'Statistical Power of McNemar Test\n'\
 'Binomial Distribution ($p$=0.5)\n'\
 'Various Effect Sizes (Percentage of Original Mean Value)'
 contour_plot(gauss, effect, power, ylabel='Effect Size', 
-             title=title, save_label='mcnemar_binomial')
+             title=title, save_label='binomial_mcnemar')
+
+
+
+
+
+
+
+
+# Exponential Distribution
+# Create test class
+exp = Test()
+
+# Distribution is Exponential
+exp.dist = lambda m,n : np.random.exponential(scale=m, size=(n,))
+# Two sample KS hypothesis test
+exp.hypothesis = stats.ks_2samp
+
+# Scale of exponential
+m = np.array([1])
+
+
+
+#Compute power statistic
+power = montecarlo_sweep(exp, means=m, effect_sizes=effect)
+
+# Plot results
+title = 'Statistical Power of KS Test\n'\
+'Exponential Distribution ($\lambda$=1)\n'\
+'Various Effect Sizes (Percentage of Original Scale Value)'
+contour_plot(exp, effect, power, ylabel='Effect Size',
+             title=title, save_label='exponential_ks')
+
+
+# Repeat with Wilcoxon signed rank test
+exp.hypothesis = stats.wilcoxon
+power = montecarlo_sweep(exp, means=m, effect_sizes=effect)
+
+# Plot results
+title = 'Statistical Power of Wilcoxon Signed-Rank Test\n'\
+'Exponential Distribution ($\lambda$=1)\n'\
+'Various Effect Sizes (Percentage of Original Scale Value)'
+contour_plot(exp, effect, power, ylabel='Effect Size', 
+             save_label='exponential_wilcoxon', title=title)
+
+
+# Repeat with Mann-Whitney U test
+exp.hypothesis = stats.mannwhitneyu
+power = montecarlo_sweep(exp, means=m, effect_sizes=effect)
+
+# Plot results
+title = 'Statistical Power of Mann-Whitney U Test\n'\
+'Exponential Distribution ($\lambda$=1)\n'\
+'Various Effect Sizes (Percentage of Original Scale Value)'
+contour_plot(exp, effect, power, ylabel='Effect Size', 
+             save_label='exponential_mannwhitney', title=title)
+
+
+
+# Plot results
+exp.hypothesis = stats.kruskal
+power = montecarlo_sweep(exp, means=m, effect_sizes=effect)
+
+title = 'Statistical Power of Kruskal-Wallis H Test\n'\
+'Exponential Distribution ($\lambda$=1)\n'\
+'Various Effect Sizes (Percentage of Original Scale Value)'
+contour_plot(exp, effect, power, ylabel='Effect Size', 
+             save_label='exponential_kruskalwallis', title=title)
